991fcd6ae03b43ab387a37a3633c394646d6515a
smae101 <batistilsheila@gmail.com>
2015-10-16 12:53:02 +0800

    Fix Donations error

--- app/src/main/java/citu/teknoybuyandselladmin/DonationsActivity.java
+++ app/src/main/java/citu/teknoybuyandselladmin/DonationsActivity.java
@@ -42,32 +42,32 @@ public class DonationsActivity extends BaseActivity {
                 ListView lv = (ListView) findViewById(R.id.listViewDonations);
                 TextView txtMessage = (TextView) findViewById(R.id.txtMessage);
 
-                if (jsonArray.length() == 0) {
-                    txtMessage.setText("No donate requests available");
-                    txtMessage.setVisibility(View.VISIBLE);
-                    lv.setVisibility(View.GONE);
-                } else {
-                    request = DonateApproval.asList(jsonArray);
-                    DonateApprovalAdapter listAdapter = new DonateApprovalAdapter(DonationsActivity.this, R.layout.activity_item, request);
-                    lv.setAdapter(listAdapter);
-                    lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                        @Override
-                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                            DonateApproval donate = (DonateApproval) parent.getItemAtPosition(position);
-                            int itemId = donate.getItemId();
-                            int requestId = donate.getRequestId();
-
-                            Intent intent;
-                            intent = new Intent(DonationsActivity.this, DonationsDetailActivity.class);
-                            intent.putExtra("itemId", itemId);
-                            intent.putExtra("requestId", requestId);
-                            startActivity(intent);
-
-                        }
-                    });
-                }
                 try {
                     jsonArray = new JSONArray(responseBody);
+                    if (jsonArray.length() == 0) {
+                        txtMessage.setText("No donate requests available");
+                        txtMessage.setVisibility(View.VISIBLE);
+                        lv.setVisibility(View.GONE);
+                    } else {
+                        request = DonateApproval.asList(jsonArray);
+                        DonateApprovalAdapter listAdapter = new DonateApprovalAdapter(DonationsActivity.this, R.layout.activity_item, request);
+                        lv.setAdapter(listAdapter);
+                        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                DonateApproval donate = (DonateApproval) parent.getItemAtPosition(position);
+                                int itemId = donate.getItemId();
+                                int requestId = donate.getRequestId();
+
+                                Intent intent;
+                                intent = new Intent(DonationsActivity.this, DonationsDetailActivity.class);
+                                intent.putExtra("itemId", itemId);
+                                intent.putExtra("requestId", requestId);
+                                startActivity(intent);
+                            }
+                        });
+                     }
+
                 } catch (JSONException e1) {
                     e1.printStackTrace();
                 }
