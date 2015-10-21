bc2485d9ef902e008fbdac5ee26e1b6368844cc6
Jacquelyn Amaya <amaya.jacque@gmail.com>
2015-10-21 13:18:46 +0800

    Removed unused menus

--- app/src/main/java/citu/teknoybuyandselladmin/ReservedDetailActivity.java
+++ app/src/main/java/citu/teknoybuyandselladmin/ReservedDetailActivity.java
@@ -188,22 +188,6 @@ public class ReservedDetailActivity extends BaseActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_reserved_detail, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public boolean checkItemClicked(MenuItem menuItem) {
         return menuItem.getItemId() != R.id.nav_reserved_items;
     }
